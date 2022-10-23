ec9b09b148fac35d2247d5a3ba90fdf43f9d4d84
LemonCaramel <admin@caramel.moe>
2022-10-23 14:39:42 +0900

    fix: IME wasn't enabled on some EditBox (test)

--- src/main/java/jp/axer/cocoainput/mixin/EditBoxMixin.java
+++ src/main/java/jp/axer/cocoainput/mixin/EditBoxMixin.java
@@ -4,6 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import jp.axer.cocoainput.wrapper.EditBoxWrapper;
 import net.minecraft.client.gui.components.EditBox;
@@ -21,6 +22,12 @@ public class EditBoxMixin {
 	 private void setFocus(boolean b,CallbackInfo ci) {
 		 wrapper.setFocused(b);
 	 }
+	 // caramel start - what is this?
+	 @Inject(method="changeFocus",at=@At("HEAD"))
+	 private void changeFocus(boolean b, CallbackInfoReturnable<Boolean> cir) {
+		 wrapper.setFocused(b);
+	 }
+	 // caramel end
 	 @Inject(method="setCanLoseFocus",at=@At("HEAD"))
 	 private void setCanLoseFocus(boolean b,CallbackInfo ci) {
 		 wrapper.setCanLoseFocus(b);

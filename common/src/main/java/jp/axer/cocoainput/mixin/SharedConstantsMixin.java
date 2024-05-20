934831a4115b580e1164233fe5078a619cb0d56a
LemonCaramel <admin@caramel.moe>
2024-05-20 14:49:13 +0900

    Update to 1.20.5

--- common/src/main/java/jp/axer/cocoainput/mixin/SharedConstantsMixin.java
+++ common/src/main/java/jp/axer/cocoainput/mixin/SharedConstantsMixin.java
@@ -1,10 +1,10 @@
 package jp.axer.cocoainput.mixin;
 
+import net.minecraft.util.StringUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import net.minecraft.SharedConstants;
 
-@Mixin(SharedConstants.class)
+@Mixin(StringUtil.class)
 public final class SharedConstantsMixin {
 
     //@ModifyConstant(method="isAllowedChatCharacter",constant=@Constant(intValue=167))
